6eeb162b8b002a9ddaa3329e46d12671deb7ad59
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-08 20:07:34 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,8 +3,11 @@ def my_select(collection)
 i = 0
 new_collection = []
 collection.each do | num | 
-
+if num/2 == int 
+  new_collection << num
   i += 0 
+else
+  yield
 end
 return new_collection
 end
